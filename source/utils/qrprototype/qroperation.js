b9dc0d2d61f754affcdaeb424ce284302c2a7a93
Ansh Goyal <110151117+AnshGoyal27@users.noreply.github.com>
2022-12-23 10:59:25 +0530

    Add files via upload

---
+++ source/utils/qrprototype/qroperation.js
@@ -0,0 +1,25 @@
+const QRCode = require('qrcode');
+const { add } = require('../../database/mongodb/operations/operation');
+const { generateClassToken } = require('../jwt/jwtoperations');
+
+const QR = {
+    generator(userid){
+        const qrtoken = generateClassToken();
+        const presentdate=Date.now()
+        console.log(presentdate);
+        return(
+            QRCode.toDataURL('http://randomgoapi/'+qrtoken+'/'+presentdate)
+            .then(url => {
+                // console.log('HERE',url);
+                add(qrtoken,userid,presentdate);
+                return(url)
+            })
+            .catch(err => {
+                console.error(err)
+            })
+        )
+        
+    }
+}
+
+module.exports=QR;
